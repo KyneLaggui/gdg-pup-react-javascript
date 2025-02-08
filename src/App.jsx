2e8cd5624162485ac1c08302eb3f54a1049cf059
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2025-02-08 22:07:03 +0800

    feat: added navigation using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import AssignmentOne from "./Assignments/AssignmentOne/AssignmentOne";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<AssignmentOne />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
