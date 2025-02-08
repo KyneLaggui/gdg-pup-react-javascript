23d5331947bd10a2e29b1f5dc86112f6f061d69e
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2025-02-08 22:30:13 +0800

    feat: created a tabs instead of using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
+import React, { useState } from "react";
 import AssignmentOne from "./Assignments/AssignmentOne/AssignmentOne";
-import { Route, Routes } from "react-router-dom";
 import AssignmentTwo from "./Assignments/AssignmentTwo/AssignmentTwo";
 import AssignmentThree from "./Assignments/AssignmentThree/AssignmentThree";
 import AssignmentFour from "./Assignments/AssignmentFour/AssignmentFour";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("one");
+
+  const handleTabChange = (tab) => setSelectedTab(tab);
+
   return (
     <>
-      <Routes>
-        <Route path="/one" element={<AssignmentOne />} />
-        <Route path="/two" element={<AssignmentTwo />} />
-        <Route path="/three" element={<AssignmentThree />} />
-        <Route path="/four" element={<AssignmentFour />} />
-      </Routes>
+      <div className="tabs">
+        <button onClick={() => handleTabChange("one")}>Assignment 1</button>
+        <button onClick={() => handleTabChange("two")}>Assignment 2</button>
+        <button onClick={() => handleTabChange("three")}>Assignment 3</button>
+        <button onClick={() => handleTabChange("four")}>Assignment 4</button>
+      </div>
+
+      <div className="tab-content">
+        {selectedTab === "one" && <AssignmentOne />}
+        {selectedTab === "two" && <AssignmentTwo />}
+        {selectedTab === "three" && <AssignmentThree />}
+        {selectedTab === "four" && <AssignmentFour />}
+      </div>
     </>
   );
 }
