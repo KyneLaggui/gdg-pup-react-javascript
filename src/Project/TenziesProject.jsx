2d142b58c915274a69f41c4e63f6f9ab59d7f3ef
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2025-02-20 18:26:12 +0800

    feat: add project directory, setup, and initial styling

---
+++ src/Project/TenziesProject.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./TenziesProject.css";
+
+export default function TenziesProject() {
+    return ( 
+        <div className="project-container">
+            <main>Main component</main>
+        </div>
+    )
+}
