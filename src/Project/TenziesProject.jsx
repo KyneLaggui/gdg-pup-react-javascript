b981c5ae4cb7e23544bd77fe64fe7fbf702ec577
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2025-02-20 18:58:48 +0800

    feat: add function to generate 10 random numbers

--- src/Project/TenziesProject.jsx
+++ src/Project/TenziesProject.jsx
@@ -3,6 +3,20 @@ import "./TenziesProject.css";
 import Die from "./Die";
 
 export default function TenziesProject() {
+
+    function generateAllNewDice () {
+        const newDice = [];
+
+        for (let i = 0; i < 10; i++) {
+            const rand = Math.ceil(Math.random() * 6);
+            newDice.push(rand);
+        }
+
+        return newDice;
+    }
+
+    console.log(generateAllNewDice());
+
     return ( 
         <div className="project-container">
             <main>
